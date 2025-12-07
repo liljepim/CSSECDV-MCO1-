224bd0dbdd69dc15473719b9ad0625633a9f3ef6
liljepim <95076212+liljepim@users.noreply.github.com>
2025-12-08 00:09:22 +0800

    Not Found Page

--- app.js
+++ app.js
@@ -97,6 +97,9 @@ app.use((req, res, next) => {
 
 app.use('/', require('./server/route/main.js'));
 app.use('/', require('./server/route/auth.js'))
+app.use((req, res) => {
+    res.render('notfound', { css: ["notfound"]})
+})
 
 
 app.listen(PORT, () => {
